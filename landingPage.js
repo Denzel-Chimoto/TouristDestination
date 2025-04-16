9c1f68f6bc6e8c69ee1c08a3656da78531fb6223
Tofara <chimototofara@gmail.com>
2025-04-16 20:46:04 +0200

    Completed Project Skeleton

---
+++ landingPage.js
@@ -0,0 +1,6 @@
+function handleSubmit(event) {
+    event.preventDefault(); // Prevent the page from reloading
+
+    // You could capture data here if needed
+    alert("Thank you for your message! We'll get back to you soon.");
+}
